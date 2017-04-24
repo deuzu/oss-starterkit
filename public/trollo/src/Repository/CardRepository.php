29c9b31072d8ab6729d12217f02dff92d019fe84
shadobot <keitamohamed10@gmail.com>
2017-04-24 12:42:57 +0200

    function create

--- public/trollo/src/Repository/CardRepository.php
+++ public/trollo/src/Repository/CardRepository.php
@@ -2,6 +2,13 @@
 
 require_once('../Database/Connection.php');
 
-function insertCard($title, $description) {
-    //
+/**
+ * Create a trollo card. 
+ */
+function create($title, $description, $date) {
+    $stmt = $db->prepare("INSERT INTO card (title, description, created_at) VALUES (:title, :description, :created_at)");
+    $stmt->bindParam(':title', $title);
+    $stmt->bindParam(':description', $description);
+    $stmt->bindParam(':created_at', $date);
+    $stm->execute();
 }
