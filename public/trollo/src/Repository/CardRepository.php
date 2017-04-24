5230237a45d04221949df278f0956a65c2c924fb
BenDeb <bendebsx@gmail.com>
2017-04-24 12:35:46 +0200

    Function delete card

--- public/trollo/src/Repository/CardRepository.php
+++ public/trollo/src/Repository/CardRepository.php
@@ -5,3 +5,14 @@ require_once('../Database/Connection.php');
 function insertCard($title, $description) {
     //
 }
+
+function deleteCard($id) {
+
+    $sql = "DELETE FROM card WHERE id=$id";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Carte supprimée";
+    } else {
+        echo "Erreur : carte non supprimée : " . $conn->error;
+    }
+}
