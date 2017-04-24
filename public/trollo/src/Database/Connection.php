65079def0ff75847bb4819aa76a3e48f896341ad
BenDeb <bendebsx@gmail.com>
2017-04-24 10:56:44 +0200

    fix typo + remove try/catch

--- public/trollo/src/Database/Connection.php
+++ public/trollo/src/Database/Connection.php
@@ -4,10 +4,5 @@ $hostname = 'localhost';
 $username = 'trollo';
 $password = 'trollo';
 $dbname = 'trollo';
-try {
-    $db = new PDO(sprintf('mysql:host='%s';dbname='%s';charset=utf8mb4', $hostname, $dbname) $username, $password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo 'ça marche';
-} catch (PDOException $e) {
-    echo 'ERREUR: ' . $e->getMessage();
-}
+
+$db = new PDO(sprintf('mysql:host=%s;dbname=%s;charset=utf8mb4', $hostname, $dbname) $username, $password);
