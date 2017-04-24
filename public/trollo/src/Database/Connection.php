873f858ebe4cb4c81070f66c510a01bf778cb53a
BenDeb <bendebsx@gmail.com>
2017-04-24 11:42:34 +0200

    fix typo comma

--- public/trollo/src/Database/Connection.php
+++ public/trollo/src/Database/Connection.php
@@ -5,4 +5,4 @@ $username = 'trollo';
 $password = 'trollo';
 $dbname = 'trollo';
 
-$db = new PDO(sprintf('mysql:host=%s;dbname=%s;charset=utf8mb4', $hostname, $dbname) $username, $password);
+$db = new PDO(sprintf('mysql:host=%s;dbname=%s;charset=utf8mb4', $hostname, $dbname), $username, $password);
