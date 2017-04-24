934bd2c4acd7e08a63733712d425af13f8075e01
kyprio <kyprio@hotmail.com>
2017-04-24 12:16:33 +0200

    New EditCard

--- public/trollo/src/Views/editCard.html.php
+++ public/trollo/src/Views/editCard.html.php
@@ -1,18 +1,12 @@
-<?php
-
-$id = 42;
-$title = 'Titre';
-$description = 'Voici une description et plusieursligne.';
-?>
 <div class="form-group">
   <label for="title">Card title :</label>
-  <input type="text" class="form-control" id="title" aria-describedby="titleFormat" value="<?php echo $title ?>" >
+  <input type="text" class="form-control" id="title" aria-describedby="titleFormat" value="<?php echo $card['title'] ?>" >
   <small id="titleFormat" class="form-text text-muted">Title format : Author - Title</small>
 </div>
 <div class="form-group">
   <label for="description">Card description :</label>
-  <textarea class="form-control" id="description" rows="5" ><?php echo $description ?></textarea>
+  <textarea class="form-control" id="description" rows="5" ><?php echo $card['description'] ?></textarea>
 </div>
-<button type="submit" class="btn btn-primary">Create a card</button>
+<button type="submit" class="btn btn-primary">Modify card</button>
 <a class="btn btn-danger" href="/" role="button">Cancel</a>
 </form>
