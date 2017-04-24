14135f3d632abbbfb0b61b39c8840b6905e5c7fc
root <root@stargate>
2017-04-24 11:02:45 +0200

    add cards in homepage

--- public/trollo/src/Controller/HomepageController.php
+++ public/trollo/src/Controller/HomepageController.php
@@ -1,3 +1,5 @@
 <?php
+$cards = array ( "title" => array ("title a", "title b"),
+         "description" => array ("ceci est un test", "ceci est un test pour b"));
 
 require __DIR__.'/../Views/homepage.html.php';
