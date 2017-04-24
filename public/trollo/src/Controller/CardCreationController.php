2eba25eadfe7ecbda7a19b3732b1b63465f13142
fltou <florian.touya@gmail.com>
2017-04-24 12:38:36 +0200

    Rename controllers

--- public/trollo/src/Controller/CardCreationController.php
+++ public/trollo/src/Controller/CardCreationController.php
@@ -4,5 +4,5 @@ $title = htmlspecialchars($_POST['title'], ENT_NOQUOTES);
 $description = htmlspecialchars($_POST['description'], ENT_NOQUOTES);
 
 if(isset($title) && !empty($title) && isset($description) && !empty($description)) {
-	
+    //
 }
